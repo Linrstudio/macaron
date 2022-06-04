0b9c740d0c99d66ce71be8e1bb0328dcaea180ff
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-05 01:39:17 +0900

    Tab items

--- packages/editor/src/app/TabBar.tsx
+++ packages/editor/src/app/TabBar.tsx
@@ -1,11 +1,35 @@
+import { colors } from "@seanchas116/paintkit/src/components/Palette";
 import React from "react";
 import styled from "styled-components";
+
+const Tab = styled.div`
+  color: ${colors.label};
+  font-weight: 500;
+  height: 32px;
+  line-height: 32px;
+  padding: 0 20px;
+
+  border-right: 2px solid ${colors.separator};
+
+  &[aria-selected="true"] {
+    color: ${colors.text};
+    border-bottom: 2px solid ${colors.active};
+  }
+`;
 
 const TabBarWrap = styled.div`
   height: 32px;
   background-color: #181818;
+  display: flex;
+  font-size: 12px;
 `;
 
 export const TabBar: React.FC = () => {
-  return <TabBarWrap></TabBarWrap>;
+  return (
+    <TabBarWrap>
+      <Tab aria-selected>File1.macaron</Tab>
+      <Tab>File2.macaron</Tab>
+      <Tab>File3.macaron</Tab>
+    </TabBarWrap>
+  );
 };
