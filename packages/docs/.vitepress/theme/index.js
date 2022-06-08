08503dbbf419266dd7170690490540cd85a81d5c
Ryohei Ikegami <iofg2100@gmail.com>
2022-06-08 20:06:48 +0900

    WIP send analytics

--- packages/docs/.vitepress/theme/index.js
+++ packages/docs/.vitepress/theme/index.js
@@ -7,3 +7,58 @@ import "@fontsource/readex-pro/700.css";
 import "./custom.css";
 
 export default DefaultTheme;
+
+// document.body.insertAdjacentHTML(
+//   "beforeend",
+//   `<script async src="https://www.googletagmanager.com/gtag/js?id=G-9SL80BL0ZZ"></script>
+//   <script>
+//     window.dataLayer = window.dataLayer || [];
+//     function gtag(){dataLayer.push(arguments);}
+//     gtag('js', new Date());
+
+//     gtag('config', 'G-9SL80BL0ZZ');
+
+//     console.log('gtag loaded');
+//   </script>`
+// );
+
+const gtag = document.createElement("script");
+gtag.src = "https://www.googletagmanager.com/gtag/js?id=G-9SL80BL0ZZ";
+document.body.appendChild(gtag);
+
+const script = document.createElement("script");
+script.textContent = `    window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-9SL80BL0ZZ', {debug_mode: true});
+console.log('gtag loaded');
+`;
+document.body.appendChild(script);
+
+const pageView = () => {
+  setTimeout(() => {
+    console.log("location changed!", location.pathname, document.title);
+    window.gtag("config", "G-9SL80BL0ZZ", {
+      page_path: location.pathname,
+      page_title: document.title,
+      debug_mode: true,
+    });
+  }, 100);
+};
+
+window.history.pushState = new Proxy(window.history.pushState, {
+  apply: (target, thisArg, argArray) => {
+    // trigger here what you need
+
+    const ret = target.apply(thisArg, argArray);
+    pageView();
+    return ret;
+  },
+});
+
+window.addEventListener("pushstate", function () {
+  pageView();
+});
+window.addEventListener("popstate", function () {
+  pageView();
+});
